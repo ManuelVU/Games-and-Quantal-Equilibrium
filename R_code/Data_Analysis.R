2e3bc13856e4465f49b49eab2716bcf2d65ba023
ManuelVU <jesus_mvu@hotmail.com>
2018-02-01 18:47:01 -0600

    cambios a data analysis y data extraction

--- R_code/Data_Analysis.R
+++ R_code/Data_Analysis.R
@@ -5,8 +5,8 @@
 # Manuel
 setwd("~/Documents/Git_Projects/Games-and-Quantal-Equilibrium/R_code/functions")
 source("Data_Extraction")
-pairs<-seq(1,10)            #Pair ID
-game_id<-seq(1,10)          #Game ID
+pairs<-seq(1,20)            #Pair ID
+game_id<-seq(1,20)          #Game ID
 n_s<-rep(2,length(game_id)) #Available strategies on each game
 user<- "Manuel"             #Write your name to specify the paths for data
 data_g<-Data_Extraction(pairs,game_id,n_s,user) #Function, extracts data from files
--- R_code/Functions/Data_Extraction.R
+++ R_code/Functions/Data_Extraction.R
@@ -1,3 +1,18 @@
 Data_Extraction<-function(subjects,games,strategies,user_id){
+  if(missing(user_id)){
+    user_id<-readline(prompt="User Name:")
+  }
+  if(user_id=="Manuel"){
+    setwd("~/Documents/Git_Projects/Games-and-Quantal-Equilibrium/Data")
+  }
+  else if(user_id=="Dario"){
+    setwd("directorio con datos de dario")
+  }
+  else if(user_id=="Hrayr"){
+    setwd("directorio con datos de Hrayr")
+  }
+  else{
+    setwd(readline(prompt="Set working directory containing data files:"))
+  }
   #Under Construction
 }
